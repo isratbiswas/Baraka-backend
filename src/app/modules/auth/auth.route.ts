74bfcbb252d7f6b8a8f9ab43950f842a3c93cefb
isratbiswas <isratbiswas28@gmail.com>
2026-03-30 17:56:26 +0600

    created auth route

---
+++ src/app/modules/auth/auth.route.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/login", AuthController.login);
+router.post("/logout", AuthController.logout);
+
+export const AuthRoutes = router;
