5bd52fa302786ce74378a9320ee9175bef66315e
isratbiswas <isratbiswas28@gmail.com>
2026-03-30 18:06:12 +0600

    created auth controller file

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import { CatchAsync } from "../../utils/CatchAsync";
+import { setAuthCookie } from "../../utils/setCookies";
+import httpStatus from "http-status-codes";
+import sendResponse from "../../utils/sendResponse";
+const login = CatchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const loginInfo = await AuthService.login(req.body);
+    console.log(loginInfo);
+    setAuthCookie(res, { accessToken: loginInfo.accessToken });
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User Logged in Successfully",
+      data: loginInfo,
+    });
+  },
+);
+
+const logout = CatchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log("Logout called");
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User Logged out Successfully",
+      data: null,
+    });
+  },
+);
+
+export const AuthController = {
+  login,
+  logout,
+};
--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { AuthController } from "./auth.controller";
 
 const router = Router();
 
