0fac2b4d89d416602978aa9fdb4eb9f6f457310c
isratbiswas <isratbiswas28@gmail.com>
2026-04-02 16:16:36 +0600

    user profile retrived

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,8 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import { CatchAsync } from "../../utils/CatchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { JwtPayload } from "jsonwebtoken";
 
 const createUser = CatchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserService.createUser(req.body);
+    sendResponse(res, {
+      success: true,
+      message: "User created successfully",
+      statusCode: 201,
+      data: user,
+    });
+  },
+);
+
+const getMe = CatchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const decodedToken = req.user as JwtPayload;
+    const result = await UserService.getMe(decodedToken.userId);
+  },
 );
 
 export const UserController = {
