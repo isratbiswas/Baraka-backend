762a11e69017d2e687fc16da663fc4f78875f2df
isratbiswas <isratbiswas28@gmail.com>
2026-04-04 22:56:33 +0600

    created order model

---
+++ src/app/modules/order/order.model.ts
@@ -0,0 +1,84 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { IOrder } from "./order.interface";
+
+export interface OrderDocument extends IOrder, Document {}
+
+const orderSchema = new Schema<OrderDocument>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    items: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        name: String,
+        price: Number,
+        quantity: Number,
+        image: String,
+      },
+    ],
+
+    shippingAddress: {
+      fullName: String,
+      phone: String,
+      address: String,
+      city: String,
+      postalCode: String,
+      country: String,
+    },
+
+    paymentMethod: {
+      type: String,
+      enum: ["COD", "ONLINE"],
+      required: true,
+    },
+
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "paid", "failed"],
+      default: "pending",
+    },
+
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+
+    shippingPrice: Number,
+    taxPrice: Number,
+
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "shipped", "delivered", "cancelled"],
+      default: "pending",
+    },
+
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+
+    paidAt: Date,
+
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+
+    deliveredAt: Date,
+
+    transactionId: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Order = mongoose.model<OrderDocument>("Order", orderSchema);
